13593fcc0328b3e9206df86464645802edadcbfc
Riveroad <kanggil0825@gmail.com>
2024-03-15 10:06:04 +0900

    commit

--- Lecture/01_openai_api.py
+++ Lecture/01_openai_api.py
@@ -1,28 +1,5 @@
-# OpenAI API 사용하기
-#   platform.openai.com
-#   1.API-KET 발급
-#   2.카드 등록(VISA, MASTER) + 5.5달러(보유)
-
-
-# 라이브러리 관리
-#   1.VENV - 프로젝트 하나 관리
-#   2.Anaconda - 여러가지 프로젝트
-
-# python -m venv ./venv (가상환경 생성)
-# .\venv\Scripts\activate (가상환경 접속)
-# pip install openai (가상환경 라이브러리)
-
-
-# 챗봇 만들기
-#   - ChatGPT: 서비스 이름(ex: 카카오톡)
-#    > 인공지능 모델: GPT
-#    > 무료 : 3.5
-#    > 유료 : 4.0
-
-#   OpenAI 회사에서 GPT관련 API 제공
-#   - https://openai.com/blog/openai-api
-
 from openai import OpenAI
+
 client = OpenAI(api_key="")
 
 completion = client.chat.completions.create(
